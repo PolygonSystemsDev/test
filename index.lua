fd722869f4de7d10ec5a08cf05496c3521f3afac
PolygonSystemsDev <144558507+PolygonSystemsDev@users.noreply.github.com>
2023-11-12 09:33:50 -0600

    Create index.lua

---
+++ index.lua
@@ -0,0 +1,234 @@
+local oZkyVXaMwusKKUm = {["Frame_1"] = Instance.new("Frame"), ["UIListLayout_1"] = Instance.new("UIListLayout"), ["Frame_2"] = Instance.new("Frame"), ["TextLabel_1"] = Instance.new("TextLabel"), ["ImageLabel_1"] = Instance.new("ImageLabel"), ["Frame_3"] = Instance.new("Frame"), ["TextLabel_2"] = Instance.new("TextLabel"), ["Frame_4"] = Instance.new("Frame"), ["ImageLabel_2"] = Instance.new("ImageLabel"), ["ImageLabel_3"] = Instance.new("ImageLabel"), ["ImageLabel_4"] = Instance.new("ImageLabel"), ["Frame_5"] = Instance.new("Frame"), ["TextLabel_3"] = Instance.new("TextLabel"), ["ImageLabel_5"] = Instance.new("ImageLabel"), ["TextLabel_4"] = Instance.new("TextLabel"), ["Frame_6"] = Instance.new("Frame"), ["Frame_7"] = Instance.new("Frame"), ["ImageLabel_6"] = Instance.new("ImageLabel"), ["ImageLabel_7"] = Instance.new("ImageLabel"), ["ImageLabel_8"] = Instance.new("ImageLabel"), ["ScreenGui_1"] = Instance.new("ScreenGui")}
+
+oZkyVXaMwusKKUm.ScreenGui_1.Parent = game.Players.LocalPlayer.PlayerGui
+
+oZkyVXaMwusKKUm.Frame_1.BackgroundColor3 = Color3.new(1, 1, 1)
+oZkyVXaMwusKKUm.Frame_1.BackgroundTransparency = 1
+oZkyVXaMwusKKUm.Frame_1.BorderColor3 = Color3.new(0, 0, 0)
+oZkyVXaMwusKKUm.Frame_1.BorderSizePixel = 0
+oZkyVXaMwusKKUm.Frame_1.Position = UDim2.new(0.800000012, 0, 0.150000006, 0)
+oZkyVXaMwusKKUm.Frame_1.Size = UDim2.new(0.200000003, 0, 0.850000024, 0)
+oZkyVXaMwusKKUm.Frame_1.Name = [[Notifications]]
+oZkyVXaMwusKKUm.Frame_1.Parent = oZkyVXaMwusKKUm.ScreenGui_1
+
+oZkyVXaMwusKKUm.UIListLayout_1.Padding = UDim.new(0.0149999997, 0)
+oZkyVXaMwusKKUm.UIListLayout_1.HorizontalAlignment = Enum.HorizontalAlignment.Center
+oZkyVXaMwusKKUm.UIListLayout_1.SortOrder = Enum.SortOrder.LayoutOrder
+oZkyVXaMwusKKUm.UIListLayout_1.Parent = oZkyVXaMwusKKUm.Frame_1
+
+oZkyVXaMwusKKUm.Frame_2.BackgroundColor3 = Color3.new(0.129412, 0.129412, 0.12549)
+oZkyVXaMwusKKUm.Frame_2.BorderColor3 = Color3.new(0, 0, 0)
+oZkyVXaMwusKKUm.Frame_2.BorderSizePixel = 0
+oZkyVXaMwusKKUm.Frame_2.Size = UDim2.new(1, 0, 0.100000001, 0)
+oZkyVXaMwusKKUm.Frame_2.Name = [[Success]]
+oZkyVXaMwusKKUm.Frame_2.Parent = oZkyVXaMwusKKUm.Frame_1
+
+oZkyVXaMwusKKUm.TextLabel_1.FontFace = Font.new("rbxasset://fonts/families/GothamSSm.json", Enum.FontWeight.Bold, Enum.FontStyle.Normal)
+oZkyVXaMwusKKUm.TextLabel_1.Text = [[Success]]
+oZkyVXaMwusKKUm.TextLabel_1.TextColor3 = Color3.new(0.137255, 0.941176, 0.435294)
+oZkyVXaMwusKKUm.TextLabel_1.TextScaled = true
+oZkyVXaMwusKKUm.TextLabel_1.TextSize = 14
+oZkyVXaMwusKKUm.TextLabel_1.TextWrapped = true
+oZkyVXaMwusKKUm.TextLabel_1.TextXAlignment = Enum.TextXAlignment.Left
+oZkyVXaMwusKKUm.TextLabel_1.BackgroundColor3 = Color3.new(1, 1, 1)
+oZkyVXaMwusKKUm.TextLabel_1.BackgroundTransparency = 1
+oZkyVXaMwusKKUm.TextLabel_1.BorderColor3 = Color3.new(0, 0, 0)
+oZkyVXaMwusKKUm.TextLabel_1.BorderSizePixel = 0
+oZkyVXaMwusKKUm.TextLabel_1.Position = UDim2.new(0.200000003, 0, 0.150000006, 0)
+oZkyVXaMwusKKUm.TextLabel_1.Size = UDim2.new(0.800000012, 0, 0.219999999, 0)
+oZkyVXaMwusKKUm.TextLabel_1.Name = [[Title]]
+oZkyVXaMwusKKUm.TextLabel_1.Parent = oZkyVXaMwusKKUm.Frame_2
+
+oZkyVXaMwusKKUm.ImageLabel_1.Image = [[rbxassetid://15339026585]]
+oZkyVXaMwusKKUm.ImageLabel_1.ImageColor3 = Color3.new(0.137255, 0.941176, 0.435294)
+oZkyVXaMwusKKUm.ImageLabel_1.ScaleType = Enum.ScaleType.Fit
+oZkyVXaMwusKKUm.ImageLabel_1.AnchorPoint = Vector2.new(0, 0.5)
+oZkyVXaMwusKKUm.ImageLabel_1.BackgroundColor3 = Color3.new(1, 1, 1)
+oZkyVXaMwusKKUm.ImageLabel_1.BackgroundTransparency = 1
+oZkyVXaMwusKKUm.ImageLabel_1.BorderColor3 = Color3.new(0, 0, 0)
+oZkyVXaMwusKKUm.ImageLabel_1.BorderSizePixel = 0
+oZkyVXaMwusKKUm.ImageLabel_1.Position = UDim2.new(0.0599999987, 0, 0.5, 0)
+oZkyVXaMwusKKUm.ImageLabel_1.Size = UDim2.new(0.0799999982, 0, 1, 0)
+oZkyVXaMwusKKUm.ImageLabel_1.Name = [[Image]]
+oZkyVXaMwusKKUm.ImageLabel_1.Parent = oZkyVXaMwusKKUm.Frame_2
+
+oZkyVXaMwusKKUm.Frame_3.BackgroundColor3 = Color3.new(0.137255, 0.941176, 0.435294)
+oZkyVXaMwusKKUm.Frame_3.BorderColor3 = Color3.new(0, 0, 0)
+oZkyVXaMwusKKUm.Frame_3.BorderSizePixel = 0
+oZkyVXaMwusKKUm.Frame_3.Position = UDim2.new(0, 0, 1, 0)
+oZkyVXaMwusKKUm.Frame_3.Size = UDim2.new(1, 0, 0.0399999991, 0)
+oZkyVXaMwusKKUm.Frame_3.Name = [[Progress]]
+oZkyVXaMwusKKUm.Frame_3.Parent = oZkyVXaMwusKKUm.Frame_2
+
+oZkyVXaMwusKKUm.TextLabel_2.FontFace = Font.new("rbxasset://fonts/families/GothamSSm.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
+oZkyVXaMwusKKUm.TextLabel_2.Text = [[Authorization Successful]]
+oZkyVXaMwusKKUm.TextLabel_2.TextColor3 = Color3.new(0.137255, 0.941176, 0.435294)
+oZkyVXaMwusKKUm.TextLabel_2.TextScaled = true
+oZkyVXaMwusKKUm.TextLabel_2.TextSize = 14
+oZkyVXaMwusKKUm.TextLabel_2.TextWrapped = true
+oZkyVXaMwusKKUm.TextLabel_2.TextXAlignment = Enum.TextXAlignment.Left
+oZkyVXaMwusKKUm.TextLabel_2.BackgroundColor3 = Color3.new(1, 1, 1)
+oZkyVXaMwusKKUm.TextLabel_2.BackgroundTransparency = 1
+oZkyVXaMwusKKUm.TextLabel_2.BorderColor3 = Color3.new(0, 0, 0)
+oZkyVXaMwusKKUm.TextLabel_2.BorderSizePixel = 0
+oZkyVXaMwusKKUm.TextLabel_2.Position = UDim2.new(0.200000003, 0, 0.379999995, 0)
+oZkyVXaMwusKKUm.TextLabel_2.Size = UDim2.new(0.699999988, 0, 0.200000003, 0)
+oZkyVXaMwusKKUm.TextLabel_2.Name = [[Description]]
+oZkyVXaMwusKKUm.TextLabel_2.Parent = oZkyVXaMwusKKUm.Frame_2
+
+oZkyVXaMwusKKUm.Frame_4.BackgroundTransparency = 1
+oZkyVXaMwusKKUm.Frame_4.Position = UDim2.new(-0.00999999978, 0, 0, 0)
+oZkyVXaMwusKKUm.Frame_4.Size = UDim2.new(1, 0, 1, 0)
+oZkyVXaMwusKKUm.Frame_4.ZIndex = 0
+oZkyVXaMwusKKUm.Frame_4.Name = [[ShadowHolder]]
+oZkyVXaMwusKKUm.Frame_4.Parent = oZkyVXaMwusKKUm.Frame_2
+
+oZkyVXaMwusKKUm.ImageLabel_2.Image = [[rbxassetid://1316045217]]
+oZkyVXaMwusKKUm.ImageLabel_2.ImageColor3 = Color3.new(0, 0, 0)
+oZkyVXaMwusKKUm.ImageLabel_2.ImageTransparency = 0.8600000143051147
+oZkyVXaMwusKKUm.ImageLabel_2.ScaleType = Enum.ScaleType.Slice
+oZkyVXaMwusKKUm.ImageLabel_2.SliceCenter = Rect.new(10, 10, 118, 118)
+oZkyVXaMwusKKUm.ImageLabel_2.AnchorPoint = Vector2.new(0.5, 0.5)
+oZkyVXaMwusKKUm.ImageLabel_2.BackgroundTransparency = 1
+oZkyVXaMwusKKUm.ImageLabel_2.Position = UDim2.new(0.5, 0, 0.5, 6)
+oZkyVXaMwusKKUm.ImageLabel_2.Size = UDim2.new(1, 10, 1, 10)
+oZkyVXaMwusKKUm.ImageLabel_2.ZIndex = 0
+oZkyVXaMwusKKUm.ImageLabel_2.Name = [[UmbraShadow]]
+oZkyVXaMwusKKUm.ImageLabel_2.Parent = oZkyVXaMwusKKUm.Frame_4
+
+oZkyVXaMwusKKUm.ImageLabel_3.Image = [[rbxassetid://1316045217]]
+oZkyVXaMwusKKUm.ImageLabel_3.ImageColor3 = Color3.new(0, 0, 0)
+oZkyVXaMwusKKUm.ImageLabel_3.ImageTransparency = 0.8799999952316284
+oZkyVXaMwusKKUm.ImageLabel_3.ScaleType = Enum.ScaleType.Slice
+oZkyVXaMwusKKUm.ImageLabel_3.SliceCenter = Rect.new(10, 10, 118, 118)
+oZkyVXaMwusKKUm.ImageLabel_3.AnchorPoint = Vector2.new(0.5, 0.5)
+oZkyVXaMwusKKUm.ImageLabel_3.BackgroundTransparency = 1
+oZkyVXaMwusKKUm.ImageLabel_3.Position = UDim2.new(0.5, 0, 0.5, 6)
+oZkyVXaMwusKKUm.ImageLabel_3.Size = UDim2.new(1, 10, 1, 10)
+oZkyVXaMwusKKUm.ImageLabel_3.ZIndex = 0
+oZkyVXaMwusKKUm.ImageLabel_3.Name = [[PenumbraShadow]]
+oZkyVXaMwusKKUm.ImageLabel_3.Parent = oZkyVXaMwusKKUm.Frame_4
+
+oZkyVXaMwusKKUm.ImageLabel_4.Image = [[rbxassetid://1316045217]]
+oZkyVXaMwusKKUm.ImageLabel_4.ImageColor3 = Color3.new(0, 0, 0)
+oZkyVXaMwusKKUm.ImageLabel_4.ImageTransparency = 0.8799999952316284
+oZkyVXaMwusKKUm.ImageLabel_4.ScaleType = Enum.ScaleType.Slice
+oZkyVXaMwusKKUm.ImageLabel_4.SliceCenter = Rect.new(10, 10, 118, 118)
+oZkyVXaMwusKKUm.ImageLabel_4.AnchorPoint = Vector2.new(0.5, 0.5)
+oZkyVXaMwusKKUm.ImageLabel_4.BackgroundTransparency = 1
+oZkyVXaMwusKKUm.ImageLabel_4.Position = UDim2.new(0.5, 0, 0.5, 6)
+oZkyVXaMwusKKUm.ImageLabel_4.Size = UDim2.new(1, 10, 1, 10)
+oZkyVXaMwusKKUm.ImageLabel_4.ZIndex = 0
+oZkyVXaMwusKKUm.ImageLabel_4.Name = [[AmbientShadow]]
+oZkyVXaMwusKKUm.ImageLabel_4.Parent = oZkyVXaMwusKKUm.Frame_4
+
+oZkyVXaMwusKKUm.Frame_5.BackgroundColor3 = Color3.new(0.129412, 0.129412, 0.12549)
+oZkyVXaMwusKKUm.Frame_5.BorderColor3 = Color3.new(0, 0, 0)
+oZkyVXaMwusKKUm.Frame_5.BorderSizePixel = 0
+oZkyVXaMwusKKUm.Frame_5.Size = UDim2.new(1, 0, 0.100000001, 0)
+oZkyVXaMwusKKUm.Frame_5.Name = [[Error]]
+oZkyVXaMwusKKUm.Frame_5.Parent = oZkyVXaMwusKKUm.Frame_1
+
+oZkyVXaMwusKKUm.TextLabel_3.FontFace = Font.new("rbxasset://fonts/families/GothamSSm.json", Enum.FontWeight.Bold, Enum.FontStyle.Normal)
+oZkyVXaMwusKKUm.TextLabel_3.Text = [[Error]]
+oZkyVXaMwusKKUm.TextLabel_3.TextColor3 = Color3.new(1, 0.34902, 0.34902)
+oZkyVXaMwusKKUm.TextLabel_3.TextScaled = true
+oZkyVXaMwusKKUm.TextLabel_3.TextSize = 14
+oZkyVXaMwusKKUm.TextLabel_3.TextWrapped = true
+oZkyVXaMwusKKUm.TextLabel_3.TextXAlignment = Enum.TextXAlignment.Left
+oZkyVXaMwusKKUm.TextLabel_3.BackgroundColor3 = Color3.new(1, 1, 1)
+oZkyVXaMwusKKUm.TextLabel_3.BackgroundTransparency = 1
+oZkyVXaMwusKKUm.TextLabel_3.BorderColor3 = Color3.new(0, 0, 0)
+oZkyVXaMwusKKUm.TextLabel_3.BorderSizePixel = 0
+oZkyVXaMwusKKUm.TextLabel_3.Position = UDim2.new(0.200000003, 0, 0.150000006, 0)
+oZkyVXaMwusKKUm.TextLabel_3.Size = UDim2.new(0.800000012, 0, 0.219999999, 0)
+oZkyVXaMwusKKUm.TextLabel_3.Name = [[Title]]
+oZkyVXaMwusKKUm.TextLabel_3.Parent = oZkyVXaMwusKKUm.Frame_5
+
+oZkyVXaMwusKKUm.ImageLabel_5.Image = [[rbxassetid://15339060575]]
+oZkyVXaMwusKKUm.ImageLabel_5.ImageColor3 = Color3.new(1, 0.34902, 0.34902)
+oZkyVXaMwusKKUm.ImageLabel_5.ScaleType = Enum.ScaleType.Fit
+oZkyVXaMwusKKUm.ImageLabel_5.AnchorPoint = Vector2.new(0, 0.5)
+oZkyVXaMwusKKUm.ImageLabel_5.BackgroundColor3 = Color3.new(1, 1, 1)
+oZkyVXaMwusKKUm.ImageLabel_5.BackgroundTransparency = 1
+oZkyVXaMwusKKUm.ImageLabel_5.BorderColor3 = Color3.new(0, 0, 0)
+oZkyVXaMwusKKUm.ImageLabel_5.BorderSizePixel = 0
+oZkyVXaMwusKKUm.ImageLabel_5.Position = UDim2.new(0.0599999987, 0, 0.5, 0)
+oZkyVXaMwusKKUm.ImageLabel_5.Size = UDim2.new(0.0799999982, 0, 1, 0)
+oZkyVXaMwusKKUm.ImageLabel_5.Name = [[Image]]
+oZkyVXaMwusKKUm.ImageLabel_5.Parent = oZkyVXaMwusKKUm.Frame_5
+
+oZkyVXaMwusKKUm.TextLabel_4.FontFace = Font.new("rbxasset://fonts/families/GothamSSm.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
+oZkyVXaMwusKKUm.TextLabel_4.Text = [[Hookfunction Detected]]
+oZkyVXaMwusKKUm.TextLabel_4.TextColor3 = Color3.new(1, 0.34902, 0.34902)
+oZkyVXaMwusKKUm.TextLabel_4.TextScaled = true
+oZkyVXaMwusKKUm.TextLabel_4.TextSize = 14
+oZkyVXaMwusKKUm.TextLabel_4.TextWrapped = true
+oZkyVXaMwusKKUm.TextLabel_4.TextXAlignment = Enum.TextXAlignment.Left
+oZkyVXaMwusKKUm.TextLabel_4.BackgroundColor3 = Color3.new(1, 1, 1)
+oZkyVXaMwusKKUm.TextLabel_4.BackgroundTransparency = 1
+oZkyVXaMwusKKUm.TextLabel_4.BorderColor3 = Color3.new(0, 0, 0)
+oZkyVXaMwusKKUm.TextLabel_4.BorderSizePixel = 0
+oZkyVXaMwusKKUm.TextLabel_4.Position = UDim2.new(0.200000003, 0, 0.379999995, 0)
+oZkyVXaMwusKKUm.TextLabel_4.Size = UDim2.new(0.699999988, 0, 0.200000003, 0)
+oZkyVXaMwusKKUm.TextLabel_4.Name = [[Description]]
+oZkyVXaMwusKKUm.TextLabel_4.Parent = oZkyVXaMwusKKUm.Frame_5
+
+oZkyVXaMwusKKUm.Frame_6.BackgroundColor3 = Color3.new(1, 0.34902, 0.34902)
+oZkyVXaMwusKKUm.Frame_6.BorderColor3 = Color3.new(0, 0, 0)
+oZkyVXaMwusKKUm.Frame_6.BorderSizePixel = 0
+oZkyVXaMwusKKUm.Frame_6.Position = UDim2.new(0, 0, 1, 0)
+oZkyVXaMwusKKUm.Frame_6.Size = UDim2.new(1, 0, 0.0399999991, 0)
+oZkyVXaMwusKKUm.Frame_6.Name = [[Progress]]
+oZkyVXaMwusKKUm.Frame_6.Parent = oZkyVXaMwusKKUm.Frame_5
+
+oZkyVXaMwusKKUm.Frame_7.BackgroundTransparency = 1
+oZkyVXaMwusKKUm.Frame_7.Position = UDim2.new(-0.00999999978, 0, 0, 0)
+oZkyVXaMwusKKUm.Frame_7.Size = UDim2.new(1, 0, 1, 0)
+oZkyVXaMwusKKUm.Frame_7.ZIndex = 0
+oZkyVXaMwusKKUm.Frame_7.Name = [[ShadowHolder]]
+oZkyVXaMwusKKUm.Frame_7.Parent = oZkyVXaMwusKKUm.Frame_5
+
+oZkyVXaMwusKKUm.ImageLabel_6.Image = [[rbxassetid://1316045217]]
+oZkyVXaMwusKKUm.ImageLabel_6.ImageColor3 = Color3.new(0, 0, 0)
+oZkyVXaMwusKKUm.ImageLabel_6.ImageTransparency = 0.8600000143051147
+oZkyVXaMwusKKUm.ImageLabel_6.ScaleType = Enum.ScaleType.Slice
+oZkyVXaMwusKKUm.ImageLabel_6.SliceCenter = Rect.new(10, 10, 118, 118)
+oZkyVXaMwusKKUm.ImageLabel_6.AnchorPoint = Vector2.new(0.5, 0.5)
+oZkyVXaMwusKKUm.ImageLabel_6.BackgroundTransparency = 1
+oZkyVXaMwusKKUm.ImageLabel_6.Position = UDim2.new(0.5, 0, 0.5, 6)
+oZkyVXaMwusKKUm.ImageLabel_6.Size = UDim2.new(1, 10, 1, 10)
+oZkyVXaMwusKKUm.ImageLabel_6.ZIndex = 0
+oZkyVXaMwusKKUm.ImageLabel_6.Name = [[UmbraShadow]]
+oZkyVXaMwusKKUm.ImageLabel_6.Parent = oZkyVXaMwusKKUm.Frame_7
+
+oZkyVXaMwusKKUm.ImageLabel_7.Image = [[rbxassetid://1316045217]]
+oZkyVXaMwusKKUm.ImageLabel_7.ImageColor3 = Color3.new(0, 0, 0)
+oZkyVXaMwusKKUm.ImageLabel_7.ImageTransparency = 0.8799999952316284
+oZkyVXaMwusKKUm.ImageLabel_7.ScaleType = Enum.ScaleType.Slice
+oZkyVXaMwusKKUm.ImageLabel_7.SliceCenter = Rect.new(10, 10, 118, 118)
+oZkyVXaMwusKKUm.ImageLabel_7.AnchorPoint = Vector2.new(0.5, 0.5)
+oZkyVXaMwusKKUm.ImageLabel_7.BackgroundTransparency = 1
+oZkyVXaMwusKKUm.ImageLabel_7.Position = UDim2.new(0.5, 0, 0.5, 6)
+oZkyVXaMwusKKUm.ImageLabel_7.Size = UDim2.new(1, 10, 1, 10)
+oZkyVXaMwusKKUm.ImageLabel_7.ZIndex = 0
+oZkyVXaMwusKKUm.ImageLabel_7.Name = [[PenumbraShadow]]
+oZkyVXaMwusKKUm.ImageLabel_7.Parent = oZkyVXaMwusKKUm.Frame_7
+
+oZkyVXaMwusKKUm.ImageLabel_8.Image = [[rbxassetid://1316045217]]
+oZkyVXaMwusKKUm.ImageLabel_8.ImageColor3 = Color3.new(0, 0, 0)
+oZkyVXaMwusKKUm.ImageLabel_8.ImageTransparency = 0.8799999952316284
+oZkyVXaMwusKKUm.ImageLabel_8.ScaleType = Enum.ScaleType.Slice
+oZkyVXaMwusKKUm.ImageLabel_8.SliceCenter = Rect.new(10, 10, 118, 118)
+oZkyVXaMwusKKUm.ImageLabel_8.AnchorPoint = Vector2.new(0.5, 0.5)
+oZkyVXaMwusKKUm.ImageLabel_8.BackgroundTransparency = 1
+oZkyVXaMwusKKUm.ImageLabel_8.Position = UDim2.new(0.5, 0, 0.5, 6)
+oZkyVXaMwusKKUm.ImageLabel_8.Size = UDim2.new(1, 10, 1, 10)
+oZkyVXaMwusKKUm.ImageLabel_8.ZIndex = 0
+oZkyVXaMwusKKUm.ImageLabel_8.Name = [[AmbientShadow]]
+oZkyVXaMwusKKUm.ImageLabel_8.Parent = oZkyVXaMwusKKUm.Frame_7
+
+oZkyVXaMwusKKUm.ScreenGui_1.IgnoreGuiInset = true
+oZkyVXaMwusKKUm.ScreenGui_1.ScreenInsets = Enum.ScreenInsets.DeviceSafeInsets
+oZkyVXaMwusKKUm.ScreenGui_1.ResetOnSpawn = false
+oZkyVXaMwusKKUm.ScreenGui_1.Name = [[AuthPro]]
